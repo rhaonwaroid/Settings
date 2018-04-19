7965012442299e9c4b65e514d04b7a97435c54f8
mirime <kmirime@gmail.com>
2018-04-19 19:26:53 +0900

    -----

--- script/initialize.sh
+++ script/initialize.sh
@@ -5,7 +5,8 @@ sudo apt-get install wiringpi libasound2 libasound2-dev libsndfile1 libsndfile1-
 sudo cp ../0/boot/config.txt /boot/config.txt
 sudo cp ../0/etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 sudo cp ../0/etc/default/hostapd /etc/default/hostapd
-sudo cp ../0/etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ../0/etc/hostapd/hostapd24g.conf /etc/hostapd/hostapd24g.conf
+sudo cp ../0/etc/hostapd/hostapd5g.conf /etc/hostapd/hostapd5g.conf
 sudo cp ../0/etc/network/interfaces /etc/network/interfaces
 sudo cp ../0/etc/syslog-ng/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
 sudo cp ../0/etc/dnsmasq.conf /etc/dnsmasq.conf
--- script/wifi.sh
+++ script/wifi.sh
@@ -6,20 +6,30 @@ then
 	exit
 fi
 
-if [[ $# -lt 1 ]]
+if [[ $# -lt 2 ]]
 then
 	echo "You need to pass a wifi mode!"
 	echo "Usage:"
-	echo "sudo $0 <station|ap>"
+	echo "sudo $0 <station|ap>" "<2.4g|5g>"
 	exit
 fi
 
 _WIFI_MODE="$1"
-if [ "$_WIFI_MODE" != "station" -a "$_WIFI_MODE" != "ap" ]
+_WIFI_SPEED="$2"
+
+if [ "$_WIFI_MODE" != "station" -a "$_WIFI_MODE" != "ap"]
 then
 	echo "$_WIFI_MODE is unknown."
 	echo "Usage:"
-	echo "sudo $0 <station|ap>"
+	echo "sudo $0 <station|ap>" "<2.4g|5g>"
+	exit
+fi
+
+if [ "$_WIFI_SPEED" != "2.4g" -a "$_WIFI_SPEED" != "5g"]
+then
+	echo "$_WIFI_MODE is unknown."
+	echo "Usage:"
+	echo "sudo $0 <station|ap>" "<2.4g|5g>"
 	exit
 fi
 
@@ -27,24 +37,47 @@ cp ../0/etc/network/interfaces /etc/network/interfaces
 
 if [ "$_WIFI_MODE" = "station" ]
 then
-	cat >> /etc/network/interfaces << EOF
+	if [ "$_WIFI_SPEED" = "2.4g" ]
+	then
+		cat >> /etc/network/interfaces << EOF
 allow-hotplug wlan0
 iface wlan0 inet manual
 wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
 iface control inet dhcp
 EOF
+	else
+		cat >> /etc/network/interfaces << EOF
+allow-hotplug wlan1
+iface wlan0 inet manual
+wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
+iface robot inet dhcp
+EOF
+	fi
 	
 	systemctl disable dnsmasq
 else
-	cat >> /etc/network/interfaces << EOF
+	if [ "$_WIFI_SPEED" = "2.4g" ]
+	then
+		cat >> /etc/network/interfaces << EOF
 allow-hotplug wlan0
 iface wlan0 inet static
 address 10.1.1.1
 netmask 255.255.255.0
 network 10.1.1.0
 broadcast 10.1.1.255
-hostapd /etc/hostapd/hostapd.conf
+hostapd /etc/hostapd/hostapd24g.conf
 EOF
+	else
+		cat >> /etc/network/interfaces << EOF
+allow-hotplug wlan0
+iface wlan1 inet static
+address 10.1.1.1
+netmask 255.255.255.0
+network 10.1.1.0
+broadcast 10.1.1.255
+hostapd /etc/hostapd/hostapd5g.conf
+EOF
+	fi
 	
 	systemctl enable dnsmasq
 fi
